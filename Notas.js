873518f4394a9648f926ae1fc13775619fa3de0a
ValDev <al.val.tye@gmail.com>
2025-07-01 22:38:33 -0600

    Dia 2

--- Notas.js
+++ Notas.js
@@ -26,7 +26,7 @@ b_mat--;                    //Resta 1 al valor de la variable, esto es igual a: 
 
 a_mat = 1;
 
-a_mat += 2;                 //Añade el valor de un numero al valor de la variable, esto es igual a: a_mat = a_mat + 2;
+a_mat += 2;                 //Añade el valor de un numero al valor de la variable, esto es igual a: a_mat = a_mat + 2; Este operador puede ser usado con cadenas de caracteres, lo que concatena dos cadenas.
 a_mat -= 2;                 //Resta el valor de un numero al valor de la variable, esto es igual a: a_mat = a_mat - 2;
 a_mat *= 1;                 //Multiplica el valor de un numero con el valor de la variable, esto es igual a: a_mat = a_mat * 1;
 a_mat /= 1;                 //Divide el valor de la variable entre el valor de un numero, esto es igual a: a_mat = a_mat / 1;
@@ -47,10 +47,23 @@ b_char = "\\t La barra invertida con una letra 't', hace un espaciado con tabula
 b_char = "\\r La barra invertida con una letra 'r', hace un retorno de carro"
 b_char = "\\b La barra invertida con una letra 'b', hace un retroceso"
 b_char = "\\f La barra invertida con una letra 'f', fuente de formulario"
-a_char = "FirstLine\n\t\\SecondLine\nThirdLine";                                                       
+b_char = "Hola y ";
+a_char = "adios";
 
-//console.log(b_char);
+//console.log(b_char + a_char); //se pueden concatenar cadenas de caracteres con un signo "+"
+//console.log(a_char.length);   //Con la funcion "length" puedes obetener la longitud de una cadena de caracteres, en una variable de tipo string.
+//console.log(a_char[3]);       //Se puede obtener el valor en una posicion especifica de una cadena de caracteres, haciendo uso de los corchetes despues de la variable y, con un el numero de la posicion entre ellos.
+//                              //La funcion anterior no puede usarse para sustituir valores en las posisciones de una cadena de caracteres, este tipo de variables tiene la caracteristica de inmutabilidad, si se quiere hacer esto, es necesario re-asignar el valor completo de la variables
+//console.log(a_char[a_char.length - 1]); //Usando en conjunto la funcion length y restando un valor determinado, podemos obtener el valor en una posicion de la cadena, pero empezando de la dercha a la izquierda
 
 
+//Arreglos
 
-//
+const miArreglo = [[1,"a"],["b","c","d"],2];//Una variable puede ser simple o de arreglo, a su vez puede ser arreglo de 1 o multidimensional
+let auxArreglo;
+//console.log(miArreglo[0][1]);             //Para acceder al valor de la variable, se agregan corchetes a continuacion del nombre de la variable, estos corchetes van en funcion de la dimension del arreglo, el primer corchete seria para las filas, y las siguiente para las columnas, para este caso
+
+miArreglo.push(["e",3]);                    //Con la funcion push, puedes añadir valores al final de un arreglo
+auxArreglo = miArreglo.pop();               //Con la funcion pop, puedes extraer el ultimo valor de un arreglo y lo devuelve, haciendo que se pueda guardar en otra variable
+auxArreglo = miArreglo.shift();             //Con la funcion shift, puedes extraer el primer valor en un arreglo y lo devuelve
+miArreglo.unshift(auxArreglo);              //Con la funcion unshift, puedes añadir valores al inicio de un arreglo
---
+++ Reto1.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Cambia solo el código debajo de esta línea
+const wordBlanks = "The " + myAdjective+ " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Cambia esta línea
+console.log(wordBlanks);
+// Cambia solo el código encima de esta línea
